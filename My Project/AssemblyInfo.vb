0eb1929bd41e4d76aaeb34c346c6d3b0e0359875
Alex Fare <40654995+alexfare@users.noreply.github.com>
2024-07-02 13:04:14 -0400

    v5.2.1.24
    Fixed glitch going from GageList to Admin Menu and back.
    GTMenu no longer resizable.
    Admin Menu no longer resizable.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("5.1.1.23")>
-<Assembly: AssemblyFileVersion("5.1.1.23")>
+<Assembly: AssemblyVersion("5.2.1.24")>
+<Assembly: AssemblyFileVersion("5.2.1.24")>

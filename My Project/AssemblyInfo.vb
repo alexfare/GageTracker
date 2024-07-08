1aed566851fc92bfe9a50ee18e8e074e06234685
Alex Fare <40654995+alexfare@users.noreply.github.com>
2024-07-08 11:09:17 -0400

    v5.3.1.25
    Updated GTMenu, Added Clear buttons for Nominal & Actual.
    Updated GTMenu, AuditLog, WIP not working yet.
    Updated GageList Menu.
    Updated DueDateCategorizer columns to auto resize.
    Updated DueDateCategorizer page settings for manual resizing.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("5.2.1.24")>
-<Assembly: AssemblyFileVersion("5.2.1.24")>
+<Assembly: AssemblyVersion("5.3.1.25")>
+<Assembly: AssemblyFileVersion("5.3.1.25")>

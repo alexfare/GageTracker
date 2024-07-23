585222deaa53a39beb0c19369e6067e8ca7862ef
Alex Fare <40654995+alexfare@users.noreply.github.com>
2024-07-23 06:54:31 -0400

    v5.5.1.31
    Updated AccountManagement to clear password field.
    Updated filtering to include departments.
    Updated GageList & DueDateCategorizer dataset permissions.
    Updated GTMenu Menu Strip.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("5.5.1.30")>
-<Assembly: AssemblyFileVersion("5.5.1.30")>
+<Assembly: AssemblyVersion("5.5.1.31")>
+<Assembly: AssemblyFileVersion("5.5.1.31")>

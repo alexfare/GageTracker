e1f0bb840b8ea9e2bfea34ce9a0e65318766e4cc
Alex Fare <40654995+alexfare@users.noreply.github.com>
2024-08-13 06:58:23 -0400

    v5.9.1.38
    Updated GTMenu Menu bar, Adding Settings tab.
    Updated GTMenu, No longer stays in front.
    Removed ReportIssue button on GTMenu. (See Menu Bar).
    Updated GageList.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("5.9.0.37")>
-<Assembly: AssemblyFileVersion("5.9.0.37")>
+<Assembly: AssemblyVersion("5.9.1.38")>
+<Assembly: AssemblyFileVersion("5.9.1.38")>

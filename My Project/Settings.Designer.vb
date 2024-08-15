9b147c7967de90fba28a27703773015169f5fd2d
Alex Fare <40654995+alexfare@users.noreply.github.com>
2024-08-15 06:35:34 -0400

    v5.9.3.40
    Fixed GageList Filtering reseting after updating gage.
    Fixed AdminMenu logged in user not updating.
    Fixed AdminMenu last activity.
    Updated GageList, Show / Filter Status anchored when adjusting windows size.
    Updated database (Removed unused columns).
    Updated GageList Settings Menu order.

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -127,7 +127,7 @@ Namespace My
         
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("v5.9.2.39")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("v5.9.3.40")>  _
         Public ReadOnly Property VersionString() As String
             Get
                 Return CType(Me("VersionString"),String)

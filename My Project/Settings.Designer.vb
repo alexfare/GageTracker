f389e169ce84df0652c699e1ec3f44a3ee20c22d
Alex Fare <40654995+alexfare@users.noreply.github.com>
2024-08-26 08:23:59 -0400

    v5.11.0.42
    Updated ReportIssue UI.
    Adjusted About menu size.
    Updated website URL on GageList & GTMenu.
    Updated AdminMenu. Can now add, remove, update gages. (Doesn't update Audit)
    Fixed program open counter.
    Fixed issue allowing you to update Gages without searching. (After searching the 1st gage).
    Fixed issue allow you to delete gages without searching.

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -127,7 +127,7 @@ Namespace My
         
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("v5.10.0.41")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("v5.11.0.42")>  _
         Public ReadOnly Property VersionString() As String
             Get
                 Return CType(Me("VersionString"),String)

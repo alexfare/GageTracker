2cecd3dd58ec2a774a6a411873033890d5445fbd
Alex Fare <40654995+alexfare@users.noreply.github.com>
2024-12-11 05:32:11 -0500

    v5.15.1.63
    Added Update check for AdminMenu, This will now detect changes rather than updating when no changes were performed.
    Updated GageList filtering.
    Updated GTMenu, Added clearing confirmation.
    Updated text within GageType.
    Minor updates to connection string.
    Updated change detection for Clearing on GTMenu & AdminMenu.
    Updated AdminMenu Search detection.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("5.15.0.62")>
-<Assembly: AssemblyFileVersion("5.15.0.62")>
+<Assembly: AssemblyVersion("5.15.1.63")>
+<Assembly: AssemblyFileVersion("5.15.1.63")>
 <Assembly: NeutralResourcesLanguage("en")>
--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -127,7 +127,7 @@ Namespace My
         
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("v5.15.0.62")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("v5.15.1.63")>  _
         Public ReadOnly Property VersionString() As String
             Get
                 Return CType(Me("VersionString"),String)

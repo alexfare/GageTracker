3e3ce69a07619042b5cbf2ede49e151d9c1ce287
Alex Fare <40654995+alexfare@users.noreply.github.com>
2025-01-09 09:53:32 -0500

    v5.16.2.66
    Updated Dashboard.
    Updated GageList to add Dashboard view at start.
    Updated ReportIssue resize issue.
    Updated AccountManagement status display.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("5.16.1.65")>
-<Assembly: AssemblyFileVersion("5.16.1.65")>
+<Assembly: AssemblyVersion("5.16.2.66")>
+<Assembly: AssemblyFileVersion("5.16.2.66")>
 <Assembly: NeutralResourcesLanguage("en")>

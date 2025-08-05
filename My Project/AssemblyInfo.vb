d3facc97040d9ef2d646f32b231a3ed22b274a0f
Alex Fare <40654995+alexfare@users.noreply.github.com>
2025-08-05 06:49:22 -0400

    v5.18.5.81
    Fixed expired cert with a temp.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("5.18.5.80")>
-<Assembly: AssemblyFileVersion("5.18.5.80")>
+<Assembly: AssemblyVersion("5.18.5.81")>
+<Assembly: AssemblyFileVersion("5.18.5.81")>
 <Assembly: NeutralResourcesLanguage("en")>

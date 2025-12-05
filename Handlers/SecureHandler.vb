d0713a51f7c14e004c5f96e64e9c3254206f7f29
Alex Fare <40654995+alexfare@users.noreply.github.com>
2025-12-05 13:13:40 -0500

    Update wording

--- Handlers/SecureHandler.vb
+++ Handlers/SecureHandler.vb
@@ -66,8 +66,8 @@ Public Class SecureHandler
             Dim fullIV As String = ivPart1 & ivPart2
             Return SecureHandler.DecryptString(encryptedPassword, Encoding.UTF8.GetBytes(fullKey), Encoding.UTF8.GetBytes(fullIV))
         Catch ex As Exception
-            Logger.LogErrors("An error occurred while decrypting the password.")
-            Throw New ApplicationException("An error occurred while decrypting the password.", ex)
+            Logger.LogErrors("Error connecting to server.")
+            Throw New ApplicationException("Error connecting to server.", ex)
         End Try
     End Function
 End Class

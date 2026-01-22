4e8420847a7fdf624e0754d51add86cdef408314
Alex Fare <40654995+alexfare@users.noreply.github.com>
2026-01-22 07:35:30 -0500

    Update DatabaseHandler.vb

--- Handlers/DatabaseHandler.vb
+++ Handlers/DatabaseHandler.vb
@@ -4,9 +4,10 @@ Public Class DatabaseHandler
     Public Shared Function GetConnection() As OleDbConnection
         Try
             Dim connectionString As String = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={GlobalVars.DatabaseLocation};"
+            Logger.LogSystem($"Creating DB connection. DataSource={GlobalVars.DatabaseLocation}; FileExists={System.IO.File.Exists(GlobalVars.DatabaseLocation)}")
             Return New OleDbConnection(connectionString)
         Catch ex As Exception
-            Logger.LogSystem("Database connection error: " & ex.Message)
+            Logger.LogSystem("Database connection error: " & ex.ToString())
             Throw
         End Try
     End Function
